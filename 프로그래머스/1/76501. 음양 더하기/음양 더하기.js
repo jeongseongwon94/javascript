15b02f45f4381d6e66ade1ac979a770adf14cf6e
seongwon__ <148832721+jeongseongwon94@users.noreply.github.com>
2024-04-13 11:51:41 +0900

    [level 1] Title: 음양 더하기, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -1,10 +1,3 @@
 function solution(absolutes, signs) {
-    let array = [];
-    for(i=0; i<=signs.length-1; i++){
-        if(signs[i] === true){
-            array.push(absolutes[i])
-        }
-        else{array.push(absolutes[i] * -1)}
-    }
-    return array.reduce((acc, curr) => acc + curr , 0)
+    return absolutes.reduce((acc, curr, i) => acc + (curr * (signs[i] ? 1 : -1 )) , 0)
 }
